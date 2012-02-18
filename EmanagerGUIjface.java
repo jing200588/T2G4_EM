e038655bd8889da01f1e119710a223b25f618c64
Tan SweeKhoon <laflagaa@gmail.com>
2012-02-18 11:31:27 +0800

    Fixed resize issue.

--- EmanagerGUIjface.java
+++ EmanagerGUIjface.java
@@ -1,4 +1,5 @@
 import java.awt.event.ActionEvent;
+import java.awt.Color;
 
 import org.eclipse.jface.action.AbstractAction;
 import org.eclipse.jface.action.MenuManager;
@@ -6,6 +7,7 @@ import org.eclipse.jface.action.StatusLineManager;
 import org.eclipse.jface.action.ToolBarManager;
 import org.eclipse.jface.window.ApplicationWindow;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.ScrolledComposite;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
@@ -14,18 +16,37 @@ import org.eclipse.swt.widgets.Shell;
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.util.IPropertyChangeListener;
 import org.eclipse.jface.util.PropertyChangeEvent;
+import org.eclipse.swt.widgets.List;
+import org.eclipse.jface.viewers.ListViewer;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.RowLayout;
+import org.eclipse.swt.layout.RowData;
+import org.eclipse.swt.layout.FormLayout;
+import org.eclipse.swt.layout.FormData;
+import org.eclipse.swt.layout.FormAttachment;
+import org.eclipse.ui.forms.widgets.FormToolkit;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.wb.swt.SWTResourceManager;
+import org.eclipse.swt.events.ControlAdapter;
+import org.eclipse.swt.events.ControlEvent;
+import org.eclipse.swt.widgets.Label;
 
 
 public class EmanagerGUIjface extends ApplicationWindow {
 	private Action exitaction;
 	private Action swtguiaction;
-	//private ExitAction exitAction;
+	private Composite maincomposite;
+	private final FormToolkit formToolkit = new FormToolkit(Display.getDefault());
+	private Composite c1;
+
 	/**
 	 * Create the application window.
 	 */
 	public EmanagerGUIjface() {
 		super(null);
-		//exitAction = new ExitAction(this);
+
 		createActions();
 		addToolBar(SWT.FLAT | SWT.WRAP);
 		addMenuBar();
@@ -37,7 +58,62 @@ public class EmanagerGUIjface extends ApplicationWindow {
 	 * @param parent
 	 */
 	protected Control createContents(Composite parent) {
-		Composite container = new Composite(parent, SWT.NONE);
+		setStatus("Welcome!");
+		ScrolledComposite container = new ScrolledComposite(parent, SWT.V_SCROLL);
+		
+		container.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
+		container.setExpandHorizontal(true);
+		container.setExpandVertical(true);
+		container.setMinHeight(415);
+		container.setMinWidth(1000);
+		container.setEnabled(true);
+		container.setLayout(null);
+		{
+			maincomposite = new Composite(container, SWT.V_SCROLL);
+			maincomposite.setBounds(0, 0, 400, 415);
+			maincomposite.setSize(container.getSize());
+			maincomposite.setLayout(new GridLayout(3, false));
+			
+			c1 = new Composite(maincomposite, SWT.NONE);
+			GridData gd_c1 = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1);
+			gd_c1.heightHint = 415;
+			gd_c1.widthHint = 300;
+			
+			gd_c1.grabExcessVerticalSpace = true;
+			gd_c1.verticalAlignment = SWT.FILL;
+			gd_c1.grabExcessHorizontalSpace = true;
+			gd_c1.horizontalAlignment = SWT.FILL;
+			
+			c1.setLayoutData(gd_c1);
+			formToolkit.adapt(c1);
+			formToolkit.paintBordersFor(c1);
+			
+			Label Vseparator = new Label(maincomposite, SWT.SEPARATOR | SWT.VERTICAL);
+			GridData gd_Vseparator = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1);
+			gd_Vseparator.heightHint = 415;
+			gd_Vseparator.verticalAlignment = SWT.FILL;
+			Vseparator.setLayoutData(gd_Vseparator);
+			
+			
+			formToolkit.adapt(Vseparator, true, true);
+			{
+				Composite c2 = new Composite(maincomposite, SWT.NONE);
+				GridData gd_c2 = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1);
+				gd_c2.heightHint = 415;
+				gd_c2.widthHint = 700;
+				gd_c2.grabExcessVerticalSpace = true;
+				gd_c2.verticalAlignment = SWT.FILL;
+				gd_c2.grabExcessHorizontalSpace = true;
+				gd_c2.horizontalAlignment = SWT.FILL;
+				c2.setLayoutData(gd_c2);
+				formToolkit.adapt(c2);
+				formToolkit.paintBordersFor(c2);
+			}
+	
+		}
+		container.setContent(maincomposite);
+		container.setMinSize(maincomposite.computeSize(SWT.DEFAULT, SWT.DEFAULT));
+
 
 		return container;
 	}
@@ -54,6 +130,7 @@ public class EmanagerGUIjface extends ApplicationWindow {
 					}
 			};
 			exitaction.setAccelerator(SWT.ALT | SWT.F4);
+			exitaction.setToolTipText("Exits E-MAN");
 			exitaction.addPropertyChangeListener(new IPropertyChangeListener() {
 				public void propertyChange(PropertyChangeEvent arg0) {
 					System.exit(0);
@@ -129,15 +206,16 @@ public class EmanagerGUIjface extends ApplicationWindow {
 	 * @param newShell
 	 */
 	protected void configureShell(Shell newShell) {
+		newShell.setMinimumSize(new Point(1035, 526));
 		super.configureShell(newShell);
 		newShell.setText("E-Man");
+		newShell.setMaximized(true);
 	}
 
 	/**
 	 * Return the initial size of the window.
 	 */
 	protected Point getInitialSize() {
-		return new Point(1000, 800);
+		return new Point(1034, 526);
 	}
-
 }
