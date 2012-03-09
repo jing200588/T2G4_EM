dff3a81bdd66a5decfb050dabeadc963dd282672
Duy NguyenTruong <truongduy134@yahoo.com>
2012-03-09 12:55:07 +0800

    Add the code to return to the GUI

--- VenueView.java
+++ VenueView.java
@@ -1431,9 +1431,9 @@ public class VenueView extends Composite {
 					// Do the booking (The chosen time slot is legal)
 					BookingSystem.bookVenue(eventID, chosenVenueID, timeSlotChoiceInput);
 					
-					// Testing
-					ErrorBoardBooking.setText("The venue is successfully booked!");
-					ErrorBoardBooking.setVisible(true);
+					// Return to the main GUI
+					// Dummy input
+					EmanagerGUIjface.ReturnView(10);
 				}
 				catch(NumberFormatException exception)
 				{
