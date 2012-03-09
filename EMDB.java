f02a1846293b4ab34b440444cf57271a8ba457a9
Gerald Yeo <gerald.yeo@fusedthought.com>
2012-03-09 14:05:58 +0800

    Capacity update for venues

--- EMDB.java
+++ EMDB.java
@@ -27,6 +27,31 @@ public class EMDB extends EMDB_sqlite{
 			//TO DO: Test Schema
 			if (this.verify_table_count(false) == 0){
 				this.init();
+				
+				this.add_prepare("venue");
+				this.add_venue("DR1 (COM1-B-14B)", "", "", 6, 0, true);
+				this.add_venue("DR2 (COM1-B-14A)", "", "", 6, 0, true);
+				this.add_venue("DR3 (COM1-B-07)", "", "", 5, 0, true);
+				this.add_venue("DR4 (COM1-B-06)", "", "", 5, 0, true);
+				this.add_venue("DR5 (ICUBE-03-18)", "", "", 8, 0, true);
+				this.add_venue("DR6 (COM2-02-12)", "", "", 8, 0, true);
+				this.add_venue("DR7 (COM2-03-14)", "", "", 8, 0, true);
+				this.add_venue("DR8 (COM2-03-15)", "", "", 8, 0, true);
+				this.add_venue("DR9 (COM2-04-06)", "", "", 8, 0, true);
+				this.add_venue("DR10 (COM2-02-24)", "", "", 8, 0, true);
+				this.add_venue("DR11 (COM2-02-23)", "", "", 8, 0, true);
+				this.add_venue("Executive Classroom (COM2-04-02)", "", "", 25, 0, true);
+				this.add_venue("MR1 (COM1-03-19)", "", "", 7, 0, true);
+				this.add_venue("MR2 (COM1-03-28)", "", "", 7, 0, true);
+				this.add_venue("MR3 (COM2-02-26)", "", "", 7, 0, true);
+				this.add_venue("MR4 (COM1-01-22)", "", "", 7, 0, true);
+				this.add_venue("MR5 (COM1-01-18)", "", "", 7, 0, true);
+				this.add_venue("MR6 (AS6-05-10)", "", "", 8, 0, true);
+				this.add_venue("MR7 (ICUBE-03-01)", "", "", 12, 0, true);
+				this.add_venue("MR8 (ICUBE-03-48)", "", "", 12, 0, true);
+				this.add_venue("MR9 (ICUBE-03-49)", "", "", 12, 0, true);
+				this.add_venue("Video Conferencing Room (COM1-02-13)", "", "", 30, 0, true);
+				this.add_batch_commit();
 			};
 			
 			
