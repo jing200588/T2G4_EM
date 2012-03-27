9ca7fa389c83c2a92487f1b177fbc16895b7faaf
Gerald Yeo <gerald.yeo@fusedthought.com>
2012-03-27 20:21:30 +0800

    Model Budget settled

--- ModelBudget.java
+++ ModelBudget.java
@@ -29,22 +29,6 @@ public class ModelBudget {
 	public void saveNonOptimizedList(int id, Vector<Item> i_list) { 
 		event_id = id;
 		item_list = i_list;
-/*
-		db.connect();
-
-		db.delete_budget_list(id);
-
-
-		int count = i_list.size();
-		db.add_prepare("budget");
-		for (int i=0; i < count; i++){
-			Item current = i_list.get(i);
-			db.add_budget(event_id, current.getItem(), current.getPrice(), current.getSatisfaction_value(), current.getType(), true);		
-			//db.add_budget(event_id, current.getItem(), current.getPrice(), current.getSatisfaction_value(), current.getType(), current.getQuantity(), true);		
-		}
-		db.add_batch_commit();
-		db.disconnect();
-		*/
 		
 		db.budgetDB().deleteBudgetList(id);
 		db.budgetDB().addBudgetList(i_list, id);
@@ -61,19 +45,6 @@ public class ModelBudget {
 		event_id = id;
 		confirm_list = i_list;
 		
-		/*
-		db.connect();
-
-		int count = i_list.size();
-		db.add_prepare("budgetOptimized");
-		for (int i=0; i < count; i++){
-			Item current = i_list.get(i);
-			db.add_budget_optimized(event_id, current.getItem(), current.getPrice(), current.getSatisfaction_value(), current.getType(), true);	
-			//db.add_budget(event_id, current.getItem(), current.getPrice(), current.getSatisfaction_value(), current.getType(), current.getQuantity(), true);
-		}
-		db.add_batch_commit();
-		db.disconnect();	
-		*/
 		db.budgetDB().deleteBudgetListOptimized(id);
 		db.budgetDB().addBudgetListOptimized(i_list, id);
 		
@@ -99,10 +70,7 @@ public class ModelBudget {
 	 */
 
 	public void deleteNonOptimizeItemList(int id){
-		/*
-		db.connect();
-		db.delete_budget_list(id);
-		db.disconnect();*/
+
 		db.budgetDB().deleteBudgetList(id);
 
 	}
@@ -113,11 +81,6 @@ public class ModelBudget {
 	 * @return
 	 */
 	public Vector<Item> getNonOptimizeItemList(int id) {
-/*
-		db.connect();
-		item_list = db.get_budget_list(id, false);
-		db.disconnect();
-*/
 		
 		item_list = db.budgetDB().getBudgetList(id);
 		return item_list;
