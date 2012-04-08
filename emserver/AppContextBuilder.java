f282979ae23ae21fc0e388f2fc5d5759594fa4ba
Gerald Yeo <gerald.yeo@fusedthought.com>
2012-04-08 17:52:09 +0800

    server tweaks

--- emserver/AppContextBuilder.java
+++ emserver/AppContextBuilder.java
@@ -9,7 +9,7 @@ public class AppContextBuilder {
 	public WebAppContext build(){
 		webAppContext = new WebAppContext();
 		webAppContext.setDescriptor(webAppContext + "/WEB-INF/web.xml");
-		webAppContext.setResourceBase("./src/empages");
+		webAppContext.setResourceBase("src/empages");
 		webAppContext.setContextPath("/");
 		return webAppContext;
 	}
